a188ee08f749745f833df681e432fb9ac8fa7e0f
Dipa_11 <dipamantre97@gmail.com>
2021-07-29 19:32:20 +0530

    length of string solution file created

---
+++ strlen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+int ustrlen(char str[])
+{
+	int i=0;
+	while(str!='\0');
+	i++;
+	return i;
+}
+
+
+{
+	char str[50];
+	printf("enter string");
+	gets(str);
+	printf("%d",ustrlen(str));
+}
+
+
